d532cfcb5b2f1293c3f4f2d8791b2d12b42d7746
Sandra Leon <116546588+sandraldr27@users.noreply.github.com>
2023-04-03 00:26:49 +0200

    operators

---
+++ JavaBasicDocuments/operadores/src/com/company/Operadores.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        //Operadores Aritméticos//
+        int number1 = 2;
+        int number2 = 8;
+        int resultado1 = number1 + number2;
+        int resultado2 = number2 - number1;
+        int resultado3 = number1 * number2;
+        int resultado4 = number2 / number1;
+        int resultado5 = number2 % number1;
+
+        System.out.println("El resultado 1 es " + resultado1);
+        System.out.println("El resultado 2 es " + resultado2);
+        System.out.println("El resultado 3 es " + resultado3);
+        System.out.println("El resultado 4 es " + resultado4);
+        System.out.println("El resultado 5 es " + resultado5);
+
+        //Operadores de comparación//
+        /*
+        >       mayor
+        >=      mayor e igual
+        <       menor
+        <=      menor e igual
+        ==      igual (Comparativo)
+        =!      desigual
+        */
+
+        //Operadores lógicos o booleanos//
+        /*
+        &&      y (and)
+        ||      o (or)
+        !       negación (not)
+        */
+
+        //Operadores de asignación//
+        /*
+        =       igual (De equivalente, no de comparación)
+        +=
+        -=
+        *=
+        /=
+        %=
+         */
+
+        //Operadores de Incremento//
+        /*
+        ++
+         */
+
+        //Operadores de Decremento//
+        /*
+        --
+         */
+
+        //Concatenación//
+        /*
+        +      Orientado a cuando trabajamos con Strings y/o  númerospara concatenarlos
+         */
+
+    }
+}
